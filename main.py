220fa80d3c9286bb93a1dc6196aa6e27af75cbb3
smarinv16 <funemesanchez@gmail.com>
2023-04-20 08:40:07 -0500

    se agregan los archivos

---
+++ main.py
@@ -0,0 +1,11 @@
+from Mango import Mango
+
+fruta_mia = Mango()
+
+fruta_mia.nombre = "Mango Tomin"
+fruta_mia.cantidad = 5
+fruta_mia.estado = 'Maduro'
+fruta_mia.precio = 3500
+
+
+print(fruta_mia.nombre, fruta_mia.cantidad, fruta_mia.estado, fruta_mia.precio)
